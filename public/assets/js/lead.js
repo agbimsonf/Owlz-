68e4622967dca82f321205f3326a199b2ccf9515
gsavr <giorgiosav@me.com>
2019-03-31 15:15:48 -0400

    fixed contacting promoter at id >1

--- public/assets/js/lead.js
+++ public/assets/js/lead.js
@@ -2,12 +2,12 @@ $(".contactPromoter").on("click", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
     var PromoterId = $(this).data("id");
-    var start_date =  $("#start_date").val().trim();
-    var end_date = $("#end_date").val().trim();
-    var guests= $("#guests").val().trim();
-    var occasion= $("#occasion").val().trim();
-    var message = $("#message").val().trim();
-    var email_client = $("#email_client").val().trim();
+    var start_date =  $("#start_date"+PromoterId).val().trim();
+    var end_date = $("#end_date"+PromoterId).val().trim();
+    var guests= $("#guests"+PromoterId).val().trim();
+    var occasion= $("#occasion"+PromoterId).val().trim();
+    var message = $("#message"+PromoterId).val().trim();
+    var email_client = $("#email_client"+PromoterId).val().trim();
 
     var leads = {
       start_date: start_date,
@@ -18,8 +18,9 @@ $(".contactPromoter").on("click", function(event) {
       email_client: email_client,
       PromoterId: PromoterId
     };
-
+    $("#start_date,#end_date,#guests,#occasion,#message,#email_client").val("")
     console.log(leads);
+
     // Send the PUT request.
     $.ajax("/api/leads",{
       type: "POST",
